f9116641dd50081636c637ded80369329c6dc37f
charlotte Nyman <charlotte.a.nyman@gmail.com>
2025-01-15 11:43:53 -0500

    started frontend

---
+++ frontend/src/components/Header.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+import "../styles/Header.css";
+
+
+const Header = () => {
+  return (
+    <header className="header">
+      <nav>
+      <a href="tel:888-8-WAXINS" >888-8-WAXINS</a>
+      </nav>
+    </header>
+  );
+};
+
+export default Header;
---
+++ frontend/src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import Header from "../components/Header";
+import Footer from "../components/Footer";
+import Hero from "../components/Hero";
+import Section from"../components/Section"
+
+const Home = () => {
+  return (
+    <div>
+      
+      <h1>Welcome to My Restaurant</h1>
+      <Section id="about" title="About Us" content="Lorem ipsum..." />
+      <Section id="menu" title="Our Menu" content="Discover our delicious menu..." />
+      <Section id="contact" title="Contact Us" content="Get in touch with us!" />
+      <Footer />
+
+    </div>
+  )}
+
+  export default Home;
